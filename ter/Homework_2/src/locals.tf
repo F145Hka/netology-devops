b29941e6262d92fc25b474119dfde0e2d7ae17bc
Alexey Kutukov <alexey.kutukov@infowatch.com>
2023-12-05 13:20:03 +0300

    updated Homework_2

--- ter/Homework_2/src/locals.tf
+++ ter/Homework_2/src/locals.tf
@@ -2,7 +2,7 @@ locals {
   web_name = "netology-${var.vpc_name}-platform-${var.vm_web_instance.name}"
   db_name  = "netology-${var.vpc_name}-platform-${var.vm_db_instance.name}"
   metadata = {
-    serial-port-enable = 1
+    serial-port-enable = "${var.serial_enabled}"
     ssh_key = "ubuntu:${var.vms_ssh_root_key}"
   }
 }
--- ter/Homework_2/src/outputs.tf
+++ ter/Homework_2/src/outputs.tf
@@ -1,6 +1,6 @@
 output "ips" {
     value = {
-        web_ip = yandex_compute_instance.web.network_interface[0].nat_ip_address
-        db_ip  = yandex_compute_instance.db.network_interface[0].nat_ip_address
+        "${local.web_name}" = yandex_compute_instance.web.network_interface[0].nat_ip_address
+        "${local.db_name}"  = yandex_compute_instance.db.network_interface[0].nat_ip_address
     }
 }
