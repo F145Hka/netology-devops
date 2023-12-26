bfed64327b9f00bf1ad404d3218adb37395cf776
Alexey Kutukov <alexey.kutukov@infowatch.com>
2023-12-26 18:58:51 +0300

    added Homework_5

---
+++ ter/Homework_5/src/vpc/main.tf
@@ -0,0 +1,12 @@
+resource "yandex_vpc_network" "net_name" {
+  name = var.env_name
+}
+
+resource "yandex_vpc_subnet" "subnet_name" {
+  for_each = {for s in var.subnets: s.zone => s }
+
+  name           = "${var.env_name}-${each.value.zone}"
+  zone           = each.value.zone
+  network_id     = yandex_vpc_network.net_name.id
+  v4_cidr_blocks = [each.value.cidr]  
+}
